f86fa5b6fe0eb91edc1389619bf30c6a0b211715
David Brown <dmlb2000@gmail.com>
2015-10-18 18:47:05 -0700

    add the github plugins to docker compose and try making runit work with them

--- runit.sh
+++ runit.sh
@@ -24,16 +24,17 @@ if [ ! -e /data/bin/gerrit.war ] ; then
   mkdir -p /data/bin
   curl -L -o /data/bin/gerrit.war https://www.gerritcodereview.com/download/gerrit-$GERRIT_VERSION.war
 fi
-if [ ! -d /data/cache ] ; then
-  java -jar /data/bin/gerrit.war init -d /data --batch
-fi
-if [ ! -d /data/index ] ; then
-  java -jar /data/bin/gerrit.war reindex -d /data
-fi
+mkdir -p /data/plugins
 for plugin in $GERRIT_PLUGINS ; do
   if [ "`echo $plugin | cut -c1,2,3,4`" = "http" ] &&
      [ ! -e /data/plugins/`basename $plugin` ] ; then
     curl -o "/data/plugins/`basename $plugin`" "$plugin"
   fi
 done
+if [ ! -d /data/cache ] ; then
+  java -jar /data/bin/gerrit.war init -d /data --batch
+fi
+if [ ! -d /data/index ] ; then
+  java -jar /data/bin/gerrit.war reindex -d /data
+fi
 exec /data/bin/gerrit.sh run
